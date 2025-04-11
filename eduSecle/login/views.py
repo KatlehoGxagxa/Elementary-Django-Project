9d3a1c58d40c52c4037e08da39b22231d2e225f4
Katleho Gxagxa <84070856+KatlehoGxagxa@users.noreply.github.com>
2025-04-12 01:51:12 +0200

    Complete Django Mini Project.

---
+++ eduSecle/login/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Welcome to the eduSecle website!")
+
+def login(request):
+    return render(request, 'login/login.html')
